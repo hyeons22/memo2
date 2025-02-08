91bc54e3a659abfd864bccceab3fb5aae5e604ac
hyeon <ski0123@naver.com>
2025-02-08 18:45:01 +0900

    first commit

---
+++ src/test/java/com/example/memopractice2/MemoPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memopractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
